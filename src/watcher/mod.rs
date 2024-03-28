47c8b216622bb7e64a3ea1dd344b3f2093527be2
Sascha Effert <fermat@douglas2a.de>
2024-03-28 20:19:46 +0100

    React only on file close for write access

--- src/watcher/mod.rs
+++ src/watcher/mod.rs
@@ -1,12 +1,21 @@
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::Path;
+
+fn event_dispatcher(event: Event) {
+    if let notify::event::EventKind::Access(notify::event::AccessKind::Close(
+        notify::event::AccessMode::Write,
+    )) = event.kind
+    {
+        println!("Close of write file event: {:?}", event);
+    }
+}
 
 pub fn create_watcher(
     directory_path: &str,
 ) -> Result<RecommendedWatcher, Box<dyn std::error::Error>> {
     // Automatically select the best implementation for your platform.
     let mut dir_watcher = notify::recommended_watcher(|res| match res {
-        Ok(event) => println!("event: {:?}", event),
+        Ok(event) => event_dispatcher(event),
         Err(e) => println!("watch error: {:?}", e),
     })?;
 
